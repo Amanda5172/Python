4dc287c54261d5eda34c2681eefe243559ca9df0
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-09-15 11:49:19 +0800

    Create TT05_Filler.py

---
+++ TT05_Filler.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.color("red")
+#changes turtle colour to red
+
+turtle.begin_fill()
+for i in range(4):
+  turtle.forward(50)
+  turtle.right(90)
+
+turtle.end_fill()
+
+#square is coloured in
+
