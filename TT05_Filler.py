25658c7af867b99d20f2d39c69428aa3b6caf89b
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-09-21 21:48:02 +0800

    Update TT05_Filler.py

--- TT05_Filler.py
+++ TT05_Filler.py
@@ -1,14 +1,28 @@
 import turtle
 
-turtle.color("red")
-#changes turtle colour to red
+colors = ["red","orange","yellow","green","blue","purple","pink","brown","gray","black","white"]
+ 
+turtle.penup()
+ 
+turtle.goto(-300,200)
 
-turtle.begin_fill()
-for i in range(4):
-  turtle.forward(50)
-  turtle.right(90)
 
-turtle.end_fill()
+for i in range(11):
+    turtle.color(colors[i])
+    turtle.begin_fill()
+    for i in range (4):
+        turtle.forward(30)
+        turtle.right(90)
+    turtle.end_fill()
+    turtle.forward(40)
+ 
+turtle.goto(-300,150)
+for i in range(11):
+    turtle.color(colors[i])
+    turtle.write(colors[i])
+    turtle.forward(40)
 
-#square is coloured in
+
+
+#squares are coloured in
 
