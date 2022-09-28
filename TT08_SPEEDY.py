2fb839aa7db027c8efefd0922286f4afd7e88cef
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-09-28 17:50:10 +0800

    Update TT08_SPEEDY.py

--- TT08_SPEEDY.py
+++ TT08_SPEEDY.py
@@ -2,7 +2,21 @@ import turtle
 
 turtle.bgcolor("blue")
 
+turtle.speed(0)
 
+for i in range(300,-300,-10):
+    turtle.penup()
+    turtle.goto(-400,i)
+    turtle.pendown()
+    turtle.forward(600)
+    
+#y-axis
+for i in range(300,-300,-10):
+    turtle.penup()
+    turtle.left(90)
+    turtle.goto(i,-400)
+    turtle.pendown()
+    turtle.forward(600)
 
 
 
