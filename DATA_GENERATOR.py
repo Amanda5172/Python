3c76c6b31ae0a898a5fc80d4b4a7fdf7a3952db9
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-06 08:40:11 +0800

    Update DATA_GENERATOR.py

--- DATA_GENERATOR.py
+++ DATA_GENERATOR.py
@@ -1,1 +1,11 @@
+import random, sys
 
+data = ['integer', 'real','character','string','boolean','date']
+n = int(input("Input a number from 1 to 6: "))
+data[n]
+
+if n==1:
+    b= random.randint(0,sys.maxsize))
+
+elif n==2:
+    b = random.expovariate(lambd)
