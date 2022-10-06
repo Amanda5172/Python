1ad884efa0ece2c4e2f525ffc457b2243e9fc204
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-06 08:42:00 +0800

    Update DATA_GENERATOR.py

--- DATA_GENERATOR.py
+++ DATA_GENERATOR.py
@@ -1,11 +1,14 @@
 import random, sys
 
 data = ['integer', 'real','character','string','boolean','date']
-n = int(input("Input a number from 1 to 6: "))
-data[n]
+n = int(input("Input a number from 0 to 5: "))
 
-if n==1:
-    b= random.randint(0,sys.maxsize))
 
-elif n==2:
-    b = random.expovariate(lambd)
+if n==0:
+    b= random.randint(0,sys.maxsize)
+
+elif n==1:
+    b = random.expovariate(1)
+
+print(data[n])
+print(b)
