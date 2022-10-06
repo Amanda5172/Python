75840d9e01f61ec375754337707fb541f2166543
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-06 09:08:16 +0800

    Update DATA_GENERATOR.py

--- DATA_GENERATOR.py
+++ DATA_GENERATOR.py
@@ -4,13 +4,21 @@ data = ['integer', 'real','character','string','boolean','date']
 
 
 def integer():
-    b= random.randint(0,sys.maxsize)
+    b = random.randint(0,sys.maxsize)
+    return b
 
 def real():
     b = random.expovariate(1)
+    return b
 
 def char():
-    return chr(random.randint(48,126))
+    b = chr(random.randint(48,126))
+    return b
+
+def string():
+    n = random.randint(0,10)
+    b = 
+
 
 
     
@@ -18,18 +26,17 @@ print(data)
 u = int(input("Select a data type from the list(from 0 to 5): "))
 
 if u==0:
-    integer()
+    print(integer())
 elif u == 1:
-    real()
+    print(real())
 elif u ==2:
-    character()
+    print(char())
 elif u == 3:
-    string()
+    print(string())
 elif u == 4:
-    boolean()
+    print(boolean())
 else:
-    date()
-
+    print(date())
 
 
 
