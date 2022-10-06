5551b3731b0d0733db3942f36639af986bacf08f
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-06 08:26:12 +0800

    Update AS24_PROJECT_TIMELINE.py

--- AS24_PROJECT_TIMELINE.py
+++ AS24_PROJECT_TIMELINE.py
@@ -1,11 +1,12 @@
-import datetime
+from datetime import datetime
 
-start_date = input("Start date: ")
-end_date = input("End date: ")
-
-
-
+a = str(input("Start date(eg. Jun 1 2005): "))
+b = str(input("End date(eg. Aug 1 2005): "))
+#date must be written in format month day year, eg. Jun 1 2005
 
 
+start = datetime.strptime(a,'%b %d %Y')
+end = datetime.strptime(b,'%b %d %Y') 
 
+print(end-start)
 
