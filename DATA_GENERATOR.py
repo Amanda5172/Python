55eb28c0c6ccda1eabe33ba4a093fdcb58d89ebe
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-18 00:09:41 +0800

    Update DATA_GENERATOR.py

--- DATA_GENERATOR.py
+++ DATA_GENERATOR.py
@@ -1,9 +1,9 @@
-import random, sys, string
+import random, sys, string, datetime
 
 data = ['integer', 'real','character','string','boolean','date']
 
 
-def integer():
+def inte():
     b = random.randint(0,sys.maxsize)
     return b
 
@@ -15,26 +15,36 @@ def char():
     b = chr(random.randint(48,126))
     return b
 
-def string():
-    n = random.randint(0,10)
-    b = 
+def stri():
+    n = random.randint(5,20)
+    b = ''.join(random.choice(string.ascii_letters) for i in range(n))
+    return b
 
+def bool():
+    b = random.randint(1,2)
+    return b==1
 
+def date():
+    y = random.randint(1600,2022)
+    m = random.randint(0,12)
+    d = random.randint(0,28)
+    b = datetime.datetime(y, m, d)
+    return b
 
     
 print(data)
 u = int(input("Select a data type from the list(from 0 to 5): "))
 
 if u==0:
-    print(integer())
+    print(inte())
 elif u == 1:
     print(real())
 elif u ==2:
     print(char())
 elif u == 3:
-    print(string())
+    print(stri())
 elif u == 4:
-    print(boolean())
+    print(bool())
 else:
     print(date())
 
