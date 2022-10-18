24b6be87b6779e80da80b3729b324699386bf324
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-18 14:14:40 +0800

    Update TU13_NUMBER_LISTS.py

--- TU13_NUMBER_LISTS.py
+++ TU13_NUMBER_LISTS.py
@@ -1,1 +1,15 @@
+#Exercise 1 
+
+list = [1,2,3,4,5,6]
+print(sum(list))
+
+
+#Exercise 2
+
+list = [1,2,3,4,5,6]
+print(sum(list))
+print(min(list))
+print(max(list))
+print(len(list))
+print(sum(list)/len(list))
 
