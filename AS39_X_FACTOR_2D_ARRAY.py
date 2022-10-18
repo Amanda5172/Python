57fc39ca2ab83293348cdd44c4d6d089cf0c96fb
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-18 14:21:23 +0800

    Update AS39_X_FACTOR_2D_ARRAY.py

--- AS39_X_FACTOR_2D_ARRAY.py
+++ AS39_X_FACTOR_2D_ARRAY.py
@@ -1,1 +1,5 @@
+Use a 2d array to create a playlist system for 10 artists with 10 songs each. 
 
+Your algorithm will create a random order for the songs, 
+with the bands having a minimum distance of 2 songs. i.e. you can't play the same band again until 2 songs have passed. 
+(You do not have to play EVERY song in the system, just 20 is enough.)
