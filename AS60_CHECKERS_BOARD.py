b02365cd653cee7c7f17093eb472ecfd73739a78
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-25 22:32:42 +0800

    Update AS60_CHECKERS_BOARD.py

--- AS60_CHECKERS_BOARD.py
+++ AS60_CHECKERS_BOARD.py
@@ -1,1 +1,27 @@
+n = int(input())
+m = int(input())
 
+b = []
+
+for i in range(n):
+    g=[]
+    if i % 2 == 0:
+        for j in range (m):
+            if j % 2 == 0:
+                l = "."
+            else:
+                l = "*"
+            g.append(l)
+    else:
+        for j in range (m):
+            if j % 2 == 0:
+                l = "*"
+            else:
+                l = "."
+            g.append(l)  
+    b.append(g)
+            
+print(b)
+
+
+#n rows, m columns
