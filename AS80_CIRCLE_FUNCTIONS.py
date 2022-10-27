b4ba52a7706537cc8289844d1bc236caa0594a6e
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-27 08:55:04 +0800

    Update AS80_CIRCLE_FUNCTIONS.py

--- AS80_CIRCLE_FUNCTIONS.py
+++ AS80_CIRCLE_FUNCTIONS.py
@@ -1,1 +1,15 @@
+import #pi number place
+
+
+def area(radius):
+    a = radius * radius# * pi
+    print("Area =", a)
+
+def circumference(radius):
+    c = 2 * radius# * pi
+    print("Circumference =", c)
+
+r = int(input())
+area(r)
+circumference(r)
 
