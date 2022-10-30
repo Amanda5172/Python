c124683b8c793b409eb8e1b5683aa4d4048dcea2
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-10-30 14:31:23 +0800

    Update GC39_ROASTING_MACHINE.py

--- GC39_ROASTING_MACHINE.py
+++ GC39_ROASTING_MACHINE.py
@@ -1,1 +1,83 @@
+import random
+
+
+r1 = "You hear that? It’s the sound of me not caring."
+
+r2 = "All mistakes are fixable, yet you aren’t."
+
+r3 = "Are you at a loss for words, or did you exhaust your entire vocabulary?"
+
+
+r4 = "Honey, only thing bothering me is placed between your ears."
+
+r5 = "Every time I have a stick in my hand, you look like a pinata."
+
+r6 = "You are like a software update. Every time I see you, I immediately think “not now”."
+
+r7 = "When I look at you, I think to myself where have you been my whole life? Can you go back there?"
+
+
+r8 = "I think you just need a high five… in the face… with a chair."
+
+r9 = "When I listen to you, I think you really going to go far. I hope you stay there."
+
+r10 = "I look at you and think what a waste of two billion years of the evolution."
+
+r11 = "It would be a great day If you used a glue stick instead of Chapstick."
+
+r12 = "You are the reason why God is not talking to us anymore."
+
+r13 = "You can’t imagine how much happiness you can bring… by leaving the room."
+
+r14 = "Everyone is allowed to act stupid once, but you are abusing that privilege."
+
+r15 = "Why are you rolling your eyes? Are you looking for your brain?"
+
+
+
+r16 = "Every time I think you can’t get any dumber, you prove me wrong."
+
+r17 = "If I had a dollar every time you shut up, I would give it back as a thank you."
+
+r18 = "I don’t know what makes you so stupid, but it works."
+
+r19 = "It's hilarious how you are trying to fit your entire vocabulary into one sentence."
+
+r20 = "When I look at you, I wish I could meet you again for the first time… and walk past."
+
+r21 = "I would smack you, but I’m against animal abuse."
+
+r22 = "If I wanted to kill myself, I would simply jump from your ego to your IQ."
+
+r23 = "I can’t wait to spend my whole life without you."
+
+r24 = "Whoever told you to be yourself, gave you a bad advice."
+
+
+r25 = "Don’t worry about me. Worry about your eyebrows."
+
+r26 = "I thought of you today. It reminded me to take out the trash."
+
+r27 = "You have miles to go before you reach mediocre."
+
+r28 = "Mirrors can't talk. Lucky for you, they can't laugh either."
+
+r29 = "I'm glad to see you're not letting your education get in the way of your ignorance."
+
+r30 = "I'd agree with you, but then we'd both be wrong."
+
+
+
+r = [r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,r21,r22,r23,r24,r25,r26,r27,r28,r29,r30]
+
+n = random.randint(0,29)
+
+name = str(input("Name: "))
+
+print(name+".", r[n])
+           
+
+
+
+
 
