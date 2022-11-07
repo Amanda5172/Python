adb1575f6ed499292997fff4bf4d631605935a24
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-08 00:04:49 +0800

    Update TU03_STRING_HANDLING.py

--- TU03_STRING_HANDLING.py
+++ TU03_STRING_HANDLING.py
@@ -1,4 +1,10 @@
+#Exercise 1
+MyAge = int(input('What is your age? '))
+print('%d is a very nice age to be' % MyAge)
 
 
-
-
+#Exercise 2
+ComputerAge = 89.123
+ComputerName='Skynet'
+MyAge = int(input('What is your age? '))
+print('I am older than %d, because I am %.2f My name is %s.' % (MyAge,ComputerAge,ComputerName))
