52afbf03b2f3670d71ec132116ea9617130ceb1d
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-07 23:58:30 +0800

    Update TU05_IF_STATEMENTS.py

--- TU05_IF_STATEMENTS.py
+++ TU05_IF_STATEMENTS.py
@@ -1,1 +1,40 @@
+#Exercise 1: Paste in the following code. What happens if you:
+
+test = input("Is it raining? y/n ")
+if test == "y":
+    print("Oh dear, no football today!")
+   
+#a. Remove the indent - will not run
+test = input("Is it raining? y/n ")
+if test == "y":
+print("Oh dear, no football today!")
+
+#b. Type in a capital Y - does not return anything
+test = input("Is it raining? y/n ")
+if test == "y":
+    print("Oh dear, no football today!")
+    
+#c. Change the "y" to "Y" - will only work if the user types in Y
+test = input("Is it raining? y/n ")
+if test == "Y":
+    print("Oh dear, no football today!")
+
+
+#Exercise 2
+test = input("Is it raining? y/n ")
+if test in ["y", "Y", "yes"]:
+    print("Oh dear, no football today!")
+
+
+#Exercise 3 - needs indentation
+else:
+print("Great we can play!")
+
+#Corrected version:
+test = input("Is it raining? y/n ")
+if test in ["y", "Y", "yes"]:
+    print("Oh dear, no football today!")
+else:
+    print("Great we can play!")
+
 
