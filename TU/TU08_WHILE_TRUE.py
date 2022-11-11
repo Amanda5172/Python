87173dc1dc5ea999bd052121adf1e0027521ffe2
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-11 21:03:23 +0800

    Update TU08_WHILE_TRUE.py

--- TU/TU08_WHILE_TRUE.py
+++ TU/TU08_WHILE_TRUE.py
@@ -1,1 +1,11 @@
+#Exercise 1
+while True:
+    subject = input ('What is your favourite subject?')
+    if subject == "Computer Science"  :
+        break
 
+#Exercise 2
+number = 0 #You have to declare this first
+while number < 15.0 or number > 20.0 :
+    print("Please enter a number between 15 and 20: ")
+    number = float(input("enter a number:"))
