a2ed8c32297037790d7f8b91b801e8dc01612472
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-11 19:55:18 +0800

    Update TU06_IFFY_NUMBERS.py

--- TU/TU06_IFFY_NUMBERS.py
+++ TU/TU06_IFFY_NUMBERS.py
@@ -1,1 +1,8 @@
-
+MyHeight = 163
+HeightCheck = int(input("What's your height(in cm)? "))
+if MyHeight > HeightCheck:
+    print("I'm taller than you!")
+elif MyHeight==HeightCheck:
+    print("We are the same height!")
+else:
+    print("You're taller than me!")
