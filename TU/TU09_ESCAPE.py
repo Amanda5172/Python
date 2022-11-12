79e18158e440c263709aa46cfdfdf111c4eeccf8
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-12 14:07:18 +0800

    Update TU09_ESCAPE.py

--- TU/TU09_ESCAPE.py
+++ TU/TU09_ESCAPE.py
@@ -1,1 +1,12 @@
+count = 1
+while True:
+    MyNumber = input("Please enter a number: ")
+    try:
+        valid_number = int(MyNumber)
+        break
+    except ValueError:
+        print("Seriously, don't you read the instructions. \nI asked for a number...")
+    count = count + 1
+print(valid_number)
+print("It took you", count, "number of tries!")
 
