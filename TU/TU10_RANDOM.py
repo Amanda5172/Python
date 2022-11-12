ad521a5c0cf9a2ab7d4f24bf26617a1f34264938
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-12 14:21:43 +0800

    Update TU10_RANDOM.py

--- TU/TU10_RANDOM.py
+++ TU/TU10_RANDOM.py
@@ -1,1 +1,17 @@
+#Exercise 1
+import random
+
+mynumber = random.uniform(0.1, 9.9)
+print(mynumber)
+
+#Exercise 2
+import random
+Choose_Name = ["James","John","Mark","Rick","Matthew"]
+for i in range(1,5):
+    chosen = random.choice(Choose_Name)
+    print("Do you want to keep", chosen, "in the list? y/n: ")
+    r = input()
+    if r =="n":
+        Choose_Name.remove(chosen)
+print(Choose_Name)
 
