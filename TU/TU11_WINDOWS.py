450044008a6ec10fe442d410630077d23853da12
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-12 15:27:35 +0800

    Update TU11_WINDOWS.py

--- TU/TU11_WINDOWS.py
+++ TU/TU11_WINDOWS.py
@@ -1,1 +1,23 @@
+import tkinter
+import random
+
+window = tkinter.Tk()
+
+Names = ["James","John","Mark","Rick","Matthew","Luke"]
+
+def AssignedName():
+     MyRandom = random.randint(0,5)
+     
+     name.configure(text="Name: " + str(Names[MyRandom]))
+
+MyTitle = tkinter.Label(window, text="Random Name Picker",font="Georgia 16 bold")
+MyTitle.pack()
+
+MyButton = tkinter.Button(window, text="OK", command=AssignedName)
+MyButton.pack()
+
+name = tkinter.Label(window, font="Georgia 16 bold")
+name.pack()
+
+window.mainloop()
 
