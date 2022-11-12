abd07670f9422ffe2ffcb52483a62ca6f6670d56
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-12 15:58:19 +0800

    Update TU14_STRING_SLICING.py

--- TU/TU14_STRING_SLICING.py
+++ TU/TU14_STRING_SLICING.py
@@ -1,1 +1,10 @@
+#Exercise 1
+MyString = "abcdef"
+print(MyString)#abcdef
+print(MyString[0:3])#abcd
+print(MyString[3::])#def
+print(MyString[0:6:2])#ace
+print(MyString[-1:-7:-1])#fedcba
+print(MyString[0:-2]) #abcd
 
+#Exercise 2
