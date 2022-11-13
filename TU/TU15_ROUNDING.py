796b0415d0467cae0aaeb7884e162f06bd741a54
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-13 15:10:52 +0800

    Update TU15_ROUNDING.py

--- TU/TU15_ROUNDING.py
+++ TU/TU15_ROUNDING.py
@@ -1,4 +1,15 @@
+#Exercise 1
 x=3.33333333
 print(x)
-x=round(x,2)
+x=round(x,3)
 print(x)
+
+#Exercise 2
+x=3.33333333
+print(round(x,1))
+
+#Exercise 3
+mynumbers = [1.5,2.5,3.5,4.5,5,5,6.5,7.5]
+for i in mynumbers:
+    print(round(i))
+#2,2,4,4,5,5,6,8
