6577a590645a983a71038d39a1462c884f4d16a7
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-15 14:08:37 +0800

    Update TU16_F_STRINGS.py

--- TU/TU16_F_STRINGS.py
+++ TU/TU16_F_STRINGS.py
@@ -1,1 +1,15 @@
+#Exercise 1
+Ringgit = 129
+print(f"RM{Ringgit}")
+
+#Exercise 2
+numcol1 = ["Heading 1",1,2,3]
+numcol2 = ["Heading 2",6,7,8]
+
+for i in range(4):
+    print(f"{numcol1[i]:<15} {numcol2[i]:<15}")
+
+#Exercise 3
+number = int(input("Please input an integer: "))
+print(f"Binary: {number:b}")
 
