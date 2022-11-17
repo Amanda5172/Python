f3a5a311b32e062bda05cc9213aaabdf809d1496
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-17 21:19:24 +0800

    Update TT01_SAY_HELLO.py

--- TT/TT01_SAY_HELLO.py
+++ TT/TT01_SAY_HELLO.py
@@ -1,9 +1,7 @@
 # Python
 import turtle
-j=input("What would you like me to write?")
-turtle.write(j)
-turtle.forward(3.6*len(j)+10)
+turtle.write("Hello, I am a turtle!")
+turtle.forward(100)
 turtle.shape("turtle")
 turtle.done()
-
 
