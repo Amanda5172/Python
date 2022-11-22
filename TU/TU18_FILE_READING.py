7f0a37bc9fa1fb5ebad1dabb07b86f51bd6798fd
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-22 13:59:03 +0800

    Update TU18_FILE_READING.py

--- TU/TU18_FILE_READING.py
+++ TU/TU18_FILE_READING.py
@@ -4,3 +4,20 @@ with open("rudyard.txt","r") as whole_file:
     for line in whole_file:
         print(line,end="")
         n = input()
+        
+#Exercise 2
+c = input("Which poem do you want to read? 'If' or 'ma'am' ")
+
+if c == "If":
+    with open("rudyard.txt","r") as whole_file:
+        print("Press enter to read the next lines.")
+        for line in whole_file:
+            print(line,end="")
+            n = input()
+
+elif c == "ma'am":
+    with open("mam.txt","r") as whole_file:
+        print("Press enter to read the next lines.")
+        for line in whole_file:
+            print(line,end="")
+            n = input()
