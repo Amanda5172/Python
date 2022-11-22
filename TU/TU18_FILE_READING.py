469bf61d4422d2db527ac1aae6f8a02c8fd6de23
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-22 13:58:50 +0800

    Update TU18_FILE_READING.py

--- TU/TU18_FILE_READING.py
+++ TU/TU18_FILE_READING.py
@@ -1,1 +1,6 @@
-
+#Exercise 1
+with open("rudyard.txt","r") as whole_file:
+    print("Press enter to read the next lines.")
+    for line in whole_file:
+        print(line,end="")
+        n = input()
