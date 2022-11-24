048e4cf45d3685ba5e4b26ef71158b14f11abbc8
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-24 08:32:58 +0800

    Update FILE_IF_COUNT.py

--- FILE_IF_COUNT.py
+++ FILE_IF_COUNT.py
@@ -1,1 +1,23 @@
+cif=0
+cmam=0
+
+with open("if.txt","r") as ef:
+    for line in ef:
+        if " if " in line or "If " in line:
+            cif = cif+1
+            
+with open("mam.txt","r") as ef:
+    for line in ef:
+        if " if " in line or "If " in line:
+            cmam = cmam+1
+
+with open("if.txt","a") as kf:
+    kf.write(str(cif))
+with open("mam.txt","a") as kf:
+    kf.write(str(cmam))
+
+if cif > cmam:
+    print("The word 'if' appears more in 'if' than 'ma'am'")
+elif cif < cmam:
+    print("The word 'if' appears more in 'ma'am' than 'if'")
 
