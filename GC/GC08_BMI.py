c91784678847be639121c0cfa0aa4aa66c6721df
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-24 21:08:54 +0800

    Update GC08_BMI.py

--- GC/GC08_BMI.py
+++ GC/GC08_BMI.py
@@ -1,1 +1,15 @@
+weight = float(input("Weight in kilograms: "))
+height = float(input("Height in metres: "))
+
+bmi = weight / (height**2)
+
+print(bmi)
+
+
+if bmi<18.5:
+    print("Underweight")
+elif 18.5<=bmi<25:
+    print("Normal weight")
+elif 25<=bmi<30:
+    print("Obese")
 
