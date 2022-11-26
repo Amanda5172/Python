03e21e6d3f2a0c6ce26cb65eaa3885c724f244ae
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-26 23:32:45 +0800

    Create GC26_DRUNKOMETER.py

---
+++ GC/GC26_DRUNKOMETER.py
@@ -0,0 +1,11 @@
+k=input("Type 'pazazz zaqazip pizzazzes zyzzyva': ")
+
+if k == "pazazz zaqazip pizzazzes zyzzyva":
+    k=input("Type 'pazazz zaqazip pizzazzes zyzzyva': ")
+    if k == "pazazz zaqazip pizzazzes zyzzyva":
+        print("Fit to operate machinery.")
+    else:
+        print("Not fit to operate machinery.")
+
+else:
+    print("Not fit to operate machinery.")
