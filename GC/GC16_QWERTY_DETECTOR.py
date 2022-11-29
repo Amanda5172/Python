628fbde8fd5a136e17545e3ff7d833e202ea1739
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-29 23:29:05 +0800

    Create GC16_QWERTY_DETECTOR.py

---
+++ GC/GC16_QWERTY_DETECTOR.py
@@ -0,0 +1,22 @@
+text=input("Input text: ")
+
+a=0
+
+if 'q' in text:
+    a=a+1
+if 'w' in text:
+    a=a+1
+if 'e' in text:
+    a=a+1
+if 'r' in text:
+    a=a+1
+if 't' in text:
+    a=a+1
+if 'y' in text:
+    a=a+1
+
+
+if a == 6:
+    print("Yes")
+else:
+    print("No")
