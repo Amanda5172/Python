b2033d78c2f7d257b6ccc9d03eb8cb4579c71a6a
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-11-30 17:20:39 +0800

    Update AS26_WORD_LENGTH_CALCULATOR.py

--- AS/AS26_WORD_LENGTH_CALCULATOR.py
+++ AS/AS26_WORD_LENGTH_CALCULATOR.py
@@ -1,1 +1,18 @@
+import string
+y=0
+#y is total word length
+wc=0
+#wc is word count
+
+with open("helloworld.txt", "r") as ef:
+    for line in ef:
+        s = line.translate(str.maketrans('','',string.punctuation))
+        x=s.split()
+        for t in range(len(x)):
+            if x[t] !="":
+                y=y+len(x[t])
+                wc=wc+1
+        
+
+print("Average word length:",y/wc)
 
