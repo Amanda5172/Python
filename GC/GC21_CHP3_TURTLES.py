49a1f9a02b2a7864b047767e7c721102baab6510
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-12-20 16:37:01 +0800

    Create GC21_CHP3_TURTLES.py

---
+++ GC/GC21_CHP3_TURTLES.py
@@ -0,0 +1,26 @@
+#Exercise 1
+for i in range(1000):
+    print("We like Python's turtles!")
+
+#Exercise 2
+Attributes: brand, serial number, price
+Method: input, process, output
+  
+#Exercise 3
+months=['January','February','March','April','May','June','July','August','September','October','November','December']
+
+for i in months:
+    print("One of the months of the year is %s." %i)
+
+#Exercise 4
+import turtle
+
+t=turtle.Turtle()
+t.lt(3645)
+
+#rotates left, faces north east
+
+#Exercise 5
+
+
+
