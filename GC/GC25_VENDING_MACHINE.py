72c011442d3413018fff2fc62f8f836a40ac7788
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2022-12-21 19:21:31 +0800

    Update GC25_VENDING_MACHINE.py

--- GC/GC25_VENDING_MACHINE.py
+++ GC/GC25_VENDING_MACHINE.py
@@ -8,10 +8,13 @@ for i in range(len(vm)):
 c=int(input("Number of the item you wish to purchase: "))
 
 ch=-1
-
 m=float(input("Money: "))
-
 ch=m-float(vp[c])
+
+while ch<0:
+    print("Insufficient money.")
+    m=m+float(input("Money: "))
+    ch=m-float(vp[c])
 
 d=round(ch,2)
 t=str(d).find('.')
@@ -21,23 +24,7 @@ if len(str(d)[t+1::])<2:
     d=str(d)+'0'
 if len(str(m)[r+1::])<2:
     m=str(m)+'0'
-
-
-while ch<0:
-    print("Insufficient money.")
-    m=m+float(input("Money: "))
-
-    ch=m-float(vp[c])
-
-    d=round(ch,2)
-    t=str(d).find('.')
-    r=str(m).find('.')
     
-    if len(str(d)[t::])<2:
-        d=str(d)+'0'
-    if len(str(m)[r::])<2:
-        m=str(m)+'0'
-
 print()
 print("Total money received:",m)
 if ch!=0:
