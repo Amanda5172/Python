5dd1035a82e92ee53284a751ecc4c20588f8d34d
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2023-08-17 08:58:13 +0800

    Create A267_BULLETPROOF_SEARCH.py

---
+++ A2/A267_BULLETPROOF_SEARCH.py
@@ -0,0 +1,32 @@
+#not done
+import random
+
+r = random.randint(1,111)
+
+l=[]
+
+for i in range(1,100):
+    l.append(i)
+
+def bs(listname,top,bottom,find):
+    while bottom<=top:
+        middle = (top+bottom)//2
+        if middle == find:
+            return middle
+        elif middle < find:
+            bottom = middle + 1
+        else:
+            top = middle - 1
+    return False
+
+def sort(listname):
+    pass
+    #idk what sort
+
+if bs(l,0,100,r) == False:
+    sort(l)
+    if bs(l,0,100,r)==False:
+        print("item not in")
+
+
+
