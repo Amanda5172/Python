ebfa2726c0d9971179e0ec8a14d6536b7a2ea146
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2023-09-08 11:30:46 +0800

    Update A266_BIG_O_NOTATION.py

--- A2/A266_BIG_O_NOTATION.py
+++ A2/A266_BIG_O_NOTATION.py
@@ -18,9 +18,30 @@ title('Linear Complexity')
 show()
 
 #logarithmic
+x = [2, 4, 6, 8, 10, 12]
+y = [math.log(2), math.log(4), math.log(6), math.log(8), math.log(10), math.log(12)]
+plot(x, y, 'b')
+xlabel('Inputs')
+ylabel('Steps')
+title('Logarithmic Complexity')
+show()
 
 #quadratic
+x = [2, 4, 6, 8, 10, 12]
+y = [4, 16, 36, 64, 100, 144]
+plot(x, y, 'b')
+xlabel('Inputs')
+ylabel('Steps')
+title('Quadratic Complexity')
+show()
 
 #exponential
-
+x = [2, 4, 6, 8, 10, 12]
+y = [math.exp(2), math.exp(4), math.exp(6), math.exp(8), math.exp(10), math.exp(12)]
+plot(x, y, 'b')
+xlabel('Inputs')
+ylabel('Steps')
+title('Exponential Complexity')
+show()
+#?
 #factorial
