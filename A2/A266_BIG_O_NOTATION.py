87df13535232f97bbcbedb225115088fa1c1940a
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2023-09-08 11:24:18 +0800

    Update A266_BIG_O_NOTATION.py

--- A2/A266_BIG_O_NOTATION.py
+++ A2/A266_BIG_O_NOTATION.py
@@ -1,4 +1,5 @@
 from matplotlib.pyplot import ylabel, plot, show, xlabel, title
+#constant
 x = [2, 4, 6, 8, 10, 12]
 y = [2, 2, 2, 2, 2, 2]
 plot(x, y, 'r')
@@ -6,3 +7,20 @@ xlabel('Inputs')
 ylabel('Steps')
 title('Constant Complexity')
 show()
+
+#linear
+x = [2, 4, 6, 8, 10, 12]
+y = [2, 4, 6, 8, 10, 12]
+plot(x, y, 'b')
+xlabel('Inputs')
+ylabel('Steps')
+title('Linear Complexity')
+show()
+
+#logarithmic
+
+#quadratic
+
+#exponential
+
+#factorial
