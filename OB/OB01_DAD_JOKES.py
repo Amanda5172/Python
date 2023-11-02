39f9e7893600c6bd3b135fbca4716d8b5e5ca589
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2023-11-01 18:06:44 -0700

    Create OB01_DAD_JOKES.py

---
+++ OB/OB01_DAD_JOKES.py
@@ -0,0 +1,31 @@
+import random
+text=[]
+
+class joke:
+    def __init__(self):
+        self.__prompt=""
+        self.__answer=""
+    def constructor(self, prompt, answer):
+        self.__prompt=prompt
+        self.__answer=answer
+    def getprompt(self):
+        return self.__prompt
+    def getanswer(self):
+        return self.__answer
+    
+
+with open("DadJokes.txt","r") as ef:
+    for line in ef:
+        text.append(line.strip("\n"))
+
+jokes=[]
+for i in range(0,len(text),2):
+    u=joke()
+    u.constructor(text[i],text[i+1])
+    jokes.append(u)
+
+
+i = random.randint(1,10)
+print(jokes[i].getprompt())
+userans=input()
+print(jokes[i].getanswer())
